bd14043e9dab3432e2a14c2e3d1d36e78b7e85ed
majomavizben <115664122+majomavizben@users.noreply.github.com>
2023-11-30 18:25:15 +0100

    elkezdtem vergődni

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing page</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" integrity="sha512-Avb2QiuDEEvB4bZJYdft2mNjVShBftLdPG8FJ0V7irTLQ8Uo0qcPxh4Plq7G5tGm0rU+1SPhVotteLpBERwTkw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <header>
+        <nav class="header-navbar">
+            <ul>
+                <a href="#"><i class="fa-solid fa-pizza-slice fa-xl"></i></a>
+                <a href="#">MENU</a>
+                <a href="#">DEALS</a>
+            </ul>
+        </nav>
+        <nav class="header-navbar">
+            <ul>
+                <a href="#">ORDER NOW</a>
+                <a href=" #">SIGN IN</a>
+                <a href="#"><i class="fa-solid fa-user"></i></a>
+            </ul>
+        </nav>
+    </header>
+    <section>
+        <h2>Christmas Deal!</h2>
+        <div class="content-wrapper">
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius molestias impedit unde nam libero ut doloribus minima dicta fugiat aspernatur. Facilis autem possimus porro velit placeat illo nisi pariatur cupiditate amet, suscipit non animi, quidem, delectus eaque ex! Reiciendis iure sunt eum laboriosam distinctio dolorum voluptatum pariatur alias nemo amet.</p>
+            <img src="images/christmas_pizza.jpg"></img>
+        </div>
+    </section>
+    <section>
+        <h2>Try our new double Pizza!</h2>
+        <div class="content-wrapper">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo iure magnam vitae dolor nam fuga soluta error cumque, voluptatibus sit sunt exercitationem, voluptas est? Dicta dignissimos exercitationem nesciunt, itaque ab totam modi corrupti, quos, molestiae tenetur natus nemo. Iste, veniam?</p>
+            <img src="images/double_pizza.jpg"></img>
+        </div>
+    </section>
+    <footer>
+        Email: pizza@pizzaproject.com
+
+    </footer>
+</body>
+
+</html>
