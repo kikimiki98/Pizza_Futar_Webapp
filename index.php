4f152f505197397c5b97a781301f0d9b2dfb2434
majomavizben <115664122+majomavizben@users.noreply.github.com>
2023-12-02 14:18:07 +0100

    js

--- index.php
+++ index.php
@@ -47,29 +47,7 @@
 
         </footer>
     </div>
-    <script>
-        function toggleNav() {
-            var navLinks = document.getElementById("navLinks");
-
-            if (window.innerWidth <= 768) {
-                if (navLinks.style.display === "none" || navLinks.style.display === "") {
-                    navLinks.style.animation = "slideInFromLeft 0.5s ease-in";
-                    navLinks.style.display = "flex";
-                } else {
-                    navLinks.style.animation = "slideOutToLeft 0.5s ease-out";
-                    navLinks.addEventListener("animationend", onAnimationEnd);
-                }
-            } else {
-                // If the screen size is larger, always display the navigation links
-                navLinks.style.display = "flex";
-            }
-
-            function onAnimationEnd() {
-                navLinks.removeEventListener("animationend", onAnimationEnd);
-                navLinks.style.display = "none";
-            }
-        }
-    </script>
+    <script src="script.js"></script>
 </body>
 
 </html>
--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 
+function toggleNav() {
+    var navLinks = document.getElementById("navLinks");
+
+    if (window.innerWidth <= 768) {
+        if (navLinks.style.display === "none" || navLinks.style.display === "") {
+            navLinks.style.animation = "slideInFromLeft 0.5s ease-in";
+            navLinks.style.display = "flex";
+        } else {
+            navLinks.style.animation = "slideOutToLeft 0.5s ease-out";
+            navLinks.addEventListener("animationend", onAnimationEnd);
+        }
+    } else {
+        // If the screen size is larger, always display the navigation links
+        navLinks.style.display = "flex";
+    }
+
+    function onAnimationEnd() {
+        navLinks.removeEventListener("animationend", onAnimationEnd);
+        navLinks.style.display = "none";
+    }
+}
